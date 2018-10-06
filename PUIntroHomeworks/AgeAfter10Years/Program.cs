c963169bf0804f4af2bfc4b525fec41c0035ae2c
Vesko <vesiv99@abv.bg>
2018-10-06 16:43:21 +0300

    included t1&t2&t3 from the homework for week 1.

---
+++ PUIntroHomeworks/AgeAfter10Years/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            int ageAfter10Years = age + 10;
+            Console.WriteLine("After 10 years you will be:{0}",ageAfter10Years);
+        }
+    }
+}
---
+++ PUIntroHomeworks/MyUniqueNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyUniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the last 4 digits of your faculty number here:");
+            int facNumber = int.Parse(Console.ReadLine());
+            int ageAfter10Years = age + 10;
+            double uniqueNumber = (ageAfter10Years * facNumber) / Math.PI;
+            //Console.WriteLine(uniqueNumber);
+            Console.WriteLine("After 10 years you will be {0}", ageAfter10Years + " and your unique number is " + uniqueNumber);
+        }
+    }
+}
