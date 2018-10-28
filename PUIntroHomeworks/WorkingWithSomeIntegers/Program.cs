0e69658013093df7ed279d40a382c0c1c975e3ea
Vesko <vesiv99@abv.bg>
2018-10-28 20:27:02 +0200

    Included the homework for week 4.

---
+++ PUIntroHomeworks/WorkingWithSomeIntegers/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace WorkingWithSomeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] numbers = new int[100];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(132);
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine("_________________________________________________________________________");
+            Console.WriteLine("Hello. You have 100 random integers from 0 to 132.");
+            Console.WriteLine("Choose an option by typing in the letter of the option you desire.");
+            Console.WriteLine("a. Show only the numbers with even indexes");
+            Console.WriteLine("b. Show only the odd numbers with odd indexes");
+            Console.WriteLine("c. Show only the numbers that are divided by 3 without a reminder");
+            Console.WriteLine("d. Show only the numbers that divided by 7 have 1 as a reminder");
+            Console.WriteLine("e. Show only the numbers which are in the interval between 26 and 100");
+            Console.WriteLine("f. Show only the numbers which are not in the interval between 26 and 100");
+            Console.WriteLine("__________________________________________________________________________");
+            char ans = char.Parse(Console.ReadLine());
+            switch (ans)
+
+            {
+                case 'a':
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine($"{numbers[i]} has an even index - {i}");
+                        }
+                    }
+                    break;
+
+                case 'b':
+
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (numbers[i] % 2 != 0 && i % 2 != 0)
+                        {
+                            Console.WriteLine($"{numbers[i]} is odd and has an odd index - {i}");
+                        }
+                    }
+                    break;
+
+                case 'c':
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (numbers[i] % 3 == 0)
+                        {
+                            Console.WriteLine($"{numbers[i]} is divided by 3 without a reminder");
+                        }
+                    }
+                    break;
+                case 'd':
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (numbers[i] % 7 == 1)
+                        {
+                            Console.WriteLine($"{numbers[i]} has a reminder 1 when divided by 7");
+                        }
+                    }
+                    break;
+
+                case 'e':
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (numbers[i] >= 26 && numbers[i] <= 100)
+                        {
+                            Console.WriteLine($"{numbers[i]} is in the interval between 26 and 100");
+                        }
+                    }
+                    break;
+
+                case 'f':
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (numbers[i] <= 26 || numbers[i] >= 100)
+                        {
+                            Console.WriteLine($"{numbers[i]} is not between 26 and 100");
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("The value you have entered is not valid!");
+                    break;
+            }
+        }
+
+        }
+    }
+
---
+++ PUIntroHomeworks/WorkingWithSomeLetters/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace WorkingWithSomeLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Random r = new Random();
+            string[] letters = new string[100];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = ((char)r.Next(1072, 1104)).ToString();
+                Console.WriteLine(letters[i]);
+            }
+            Console.WriteLine("_________________________________________________________________________");
+            Console.WriteLine("Здравейте. Имате редица от 100 случайни букви на кирилица.");
+            Console.WriteLine("Въведете буквата на съответната опция която желаете да бъде изпълнена.");
+            Console.WriteLine("Конзолата ще разчете въведения отговор единствено ако той е на кирилица!");
+            Console.WriteLine("_________________________________________________________________________");
+            Console.WriteLine("a. Да се отпечатат само широките гласни - а, е, о");
+            Console.WriteLine("б. Да се отпечатат само тесните гласни - ъ, и, у");
+            Console.WriteLine("в. Да се отпечатат само съставните гласни - я, ю");
+            Console.WriteLine("г. Да се отпечатат само беззвучните съгласни - п, ф, к, т, ш, с, х, ц");
+            Console.WriteLine("д. Да се отпечатат само сонорните съгласни - л, м, н, р");
+            Console.WriteLine("е. Да се отпечатат само звучните съгласни - б, в, г, д, ж, з, ч");
+            Console.WriteLine("_________________________________________________________________________");
+            string ans = Console.ReadLine();
+            Console.WriteLine("_________________________________________________________________________");
+            if (ans=="а")
+            {
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i]=="а"||letters[i]=="е"||letters[i]=="о")
+                    {
+                        Console.WriteLine($"{letters[i]} е широка гласна и има индекс - {i}");
+                    }
+                }
+            }
+            if (ans=="б")
+            {
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i] == "ъ" || letters[i] == "и" || letters[i] == "у")
+                    {
+                        Console.WriteLine($"{letters[i]} е тясна гласна и има индекс - {i}");
+                    }
+                }
+            }
+            if (ans=="в")
+            {
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i] == "я" || letters[i] == "ю")
+                    {
+                        Console.WriteLine($"{letters[i]} е съставна гласна и има индекс - {i}");
+                    }
+                }
+            }
+
+            if (ans=="г")
+            {
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i] == "п" || letters[i] == "ф" || letters[i] == "к" || letters[i] == "с" || letters[i] == "т" || letters[i] == "ш" || letters[i] == "х" || letters[i] == "ц")
+                    {
+                        Console.WriteLine($"{letters[i]} е беззвучна съгласна и има индекс - {i}");
+                    }
+                }
+            }
+
+            if (ans=="д")
+            {
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i] == "п" || letters[i] == "л" || letters[i] == "м" || letters[i] == "н" || letters[i] == "р")
+                    {
+                        Console.WriteLine($"{letters[i]} е сонорна съгласна и има индекс - {i}");
+                    }
+                }
+            }
+
+            if (ans=="е")
+            {
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i] == "б" || letters[i] == "в" || letters[i] == "г" || letters[i] == "д" || letters[i] == "ж" || letters[i] == "з" || letters[i] == "ч")
+                    {
+                        Console.WriteLine($"{letters[i]} е звучна съгласна и има индекс - {i}");
+                    }
+                }
+            }
+            
+        }
+    }
+}
